d37795d678da72378ef0e4c290f299a2a240ffc5
Max <jmksm96@gmail.com>
2021-09-20 23:25:00 +0300

    Login Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
 
-const routes: Routes = [];
+const defaultRoute = '';
+const routes: Routes = [
+  {
+    path: 'login',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('./features/login/login.module').then((m) => m.LoginModule),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,33 @@ import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import en from '@angular/common/locales/en';
 import { NgModule } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 import { en_US, NZ_I18N } from 'ng-zorro-antd/i18n';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
 import { TodoListItemComponent } from './features/todo-lists/pages/todo-list-item/todo-list-item.component';
 import { TodoListsComponent } from './features/todo-lists/todo-lists.component';
-import {NzIconModule} from 'ng-zorro-antd/icon';
-import {NzGridModule} from 'ng-zorro-antd/grid';
-import { AuthComponent } from './auth/auth.component';
-import {NzLayoutModule} from 'ng-zorro-antd/layout';
-import {NzMenuModule} from 'ng-zorro-antd/menu';
-import {NzBreadCrumbModule} from 'ng-zorro-antd/breadcrumb';
-registerLocaleData(en);
+import { LoginComponent } from './features/login/login.component';
 
+registerLocaleData(en);
 @NgModule({
-  declarations: [AppComponent, TodoListsComponent, TodoListItemComponent, AuthComponent],
+  declarations: [
+    AppComponent,
+    TodoListsComponent,
+    TodoListItemComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -44,4 +49,5 @@ registerLocaleData(en);
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
 })
+// const icons: IconDefinition[] = [UserOutline];
 export class AppModule {}
